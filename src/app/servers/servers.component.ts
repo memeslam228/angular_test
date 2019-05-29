5e213728f64de4690c0e4fb018f3cd54bbaba4de
memeslam228 <sergeyrybchenko2000@gmail.com>
2019-05-29 15:43:28 +0300

    Some Binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationsStatus = 'No Server was created!';
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationsStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
